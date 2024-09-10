9e81b8255772821c6743132aea6f9818e062c41c
Mubin Ahmad Tokhi <156637969+MubinTokhi@users.noreply.github.com>
2024-09-10 10:45:09 -0400

    data validation

--- us_visa/costants/__init__.py
+++ us_visa/costants/__init__.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from datetime import date
 
 
 DATABASE_NAME = "US_VISA"
@@ -19,6 +20,11 @@ TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME: str = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config","schema.yaml")
+
 
 
 """ 
@@ -31,5 +37,13 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+
+""" 
+Data validation related constant start with DATA_VALIDATION var name
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 
---
+++ us_visa/utils/main_utils.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import dill
+import yaml
+
+from us_visa.exception import CustomException
+from us_visa.logger import logging
+
+
+def read_yaml_file(file_path:str) ->dict:
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e,sys) from e 
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w') as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise CustomException(e,sys) from e 
